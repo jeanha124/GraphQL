dd88667703f8e70946c731c48e9cb8296b5a3ab0
jeanha124 <jeanwoo.ha@rutgers.edu>
2022-03-08 21:11:56 -0500

    "Add bodyParser"

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
 
 app.get("/", (req, res) => res.send("Hello World"));
 
+app.use(bodyParser.join());
 app.listen(5000, () => console.log('Server is running on port 5000'));
