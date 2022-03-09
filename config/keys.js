aaf4bdc4172edeb58a28163ffa745db82bfabc23
jeanha124 <jeanwoo.ha@rutgers.edu>
2022-03-08 21:31:51 -0500

    "Add mongoDB and access cluster"

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://dev:uI7tKISwn0ZBJeOv@cluster0.emz5u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+};
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const app = express();
+const db = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
 
 app.get("/", (req, res) => res.send("Hello World"));
 
 app.use(bodyParser.json());
 app.listen(5000, () => console.log('Server is running on port 5000'));
+
+mongoose
+  .connect(db)
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
