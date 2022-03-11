178f6da89608bfd562cf17e3d98291cd22e7eca0
jeanha124 <jeanwoo.ha@rutgers.edu>
2022-03-10 21:38:56 -0500

    "Add create user"

--- index.js
+++ index.js
@@ -15,7 +15,39 @@ app.use(
   })
 );
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+const router = express.Router();
+
+const createNewUser = router.post("/new", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res
+        .status(400)
+        .json({ email: "A user has already registered with this address"});
+    } else {
+      console.log(req.body);
+      const newUserObj = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+
+      newUserObj
+        .save()
+        .then(savedUser => res.json(savedUser))
+        .catch(err => console.log(err));
+    }
+  });
+});
+
+app.use("/users", createNewUser);
+
 app.listen(5000, () => console.log('Server is running on port 5000'));
 
 mongoose
