a001672bd6e87752e2e03e61bb472a2661beac73
jeanha124 <jeanwoo.ha@rutgers.edu>
2022-04-11 19:38:15 -0400

    "Fix circular query"

--- schema/user_type.js
+++ schema/user_type.js
@@ -1,13 +1,38 @@
-const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLID, GraphQLString } = graphql;
+// const graphql = require("graphql");
+// const { GraphQLObjectType, GraphQLID, GraphQLString } = graphql;
+// const UserType = new GraphQLObjectType({
+//   name: "UserType",
+//   fields: {
+//     id: { type: GraphQLID },
+//     name: { type: GraphQLString },
+//     email: { type: GraphQLString }
+//   }
+// });
+// const PostType = require(".post_type");
+
+// module.exports = UserType;
+
+const { GraphQLObjectType, GraphQLList } = require("graphql");
+const mongoose = require("mongoose");
+const User = mongoose.model("user");
+
 const UserType = new GraphQLObjectType({
   name: "UserType",
-  fields: {
+  fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    email: { type: GraphQLString }
-  }
+    email: { type: GraphQLString },
+    posts: {
+      type: new GraphQLList(require("./post_type")),
+      resolve(parentalue) {
+        return (
+          User.findById(parentValue.id)
+            .populate("posts")
+            .then(user => user.posts)
+        );
+      }
+    } 
+  })
 });
-const PostType = require(".post_type");
 
 module.exports = UserType;
